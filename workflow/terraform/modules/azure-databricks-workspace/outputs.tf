6619190bd2c92c3ff448c976f676e8023669f7b8
Chaitanya <chaithanya.kilari@ascendion.com>
2023-08-30 08:21:55 -0500

    poc files updated

--- workflow/terraform/modules/azure-databricks-workspace/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "workspace_name" {
-  value       = azurerm_databricks_workspace.module-databricks.name
-  description = "Name of the Databricks workspace"
-}
-
-output "workspace_id" {
-  value       = azurerm_databricks_workspace.module-databricks.id
-  description = "ID of the Databricks workspace"
-}
